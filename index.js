7a50e95e25b2dbd478cac526ddfa81d2786b9430
faridnamrouti <digital.flowers@hotmail.com>
2016-12-15 22:35:17 +0900

    - add copyright header  - better documentation for javascript code

--- index.js
+++ index.js
@@ -17,15 +17,37 @@
 
 const addressbook = require('./build/Release/electron-addressbook');
 const lib = {
-    getContact: function (position) {
-        return addressbook.getContact(position);
+    /**
+     * Get Contact Information from the AddressBook using it's index
+     *
+     * @param {int} [index] contact index in the Addressbook
+     * @return {Object} Contact Information
+     */
+    getContact: function (index) {
+        return addressbook.getContact(index);
     },
+    /**
+     * Get Contact Information for the logged-in user
+     *
+     * @return {Object} Contact Information
+     */
     getMe: function () {
         return addressbook.getMe();
     },
+    /**
+     * Returns the number of contacts in the AddressBook.
+     *
+     * @return {int} number of contacts in the AddressBook.
+     */
     getContactsCount: function () {
         return addressbook.getContactsCount();
     },
+    /**
+     * Get all contacts information from the AddressBook
+     *
+     * @param {Function} [onProgress] Callback provides overall process percent as an integer value between 1 to 100
+     * @param {Function} [onFinish] Callback provides an array contains all of the Addressbook contacts information
+     */
     getContacts: function (onProgress, onFinish) {
         return addressbook.getContacts(onProgress, onFinish)
     }
